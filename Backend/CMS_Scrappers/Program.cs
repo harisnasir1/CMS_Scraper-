a222b3c8cd7788ca53b0f556c32336c0adc95140
harisnasir1 <haris.nasir0101@gmail.com>
2025-08-11 12:38:01 +0100

    vercel cors

--- Backend/CMS_Scrappers/Program.cs
+++ Backend/CMS_Scrappers/Program.cs
@@ -70,7 +70,8 @@ builder.Services.AddCors(options =>
                "http://localhost:3000",
                "http://localhost:5173",
                "http://127.0.0.1:5173",
-               "https://localhost:5173"
+               "https://localhost:5173",
+               "https://cms-scraper-rvny.vercel.app"
            )
            .AllowAnyHeader()
            .AllowAnyMethod()
