830c9231c999daacec0a1d6371837d5841cb4013
harisnasir1 <haris.nasir0101@gmail.com>
2025-08-12 16:53:33 +0100

    change cors url

--- Backend/CMS_Scrappers/Program.cs
+++ Backend/CMS_Scrappers/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddCors(options =>
                "http://127.0.0.1:5173",
                "https://localhost:5173",
                "https://cms-scraper-rvny.vercel.app",
-               "https://cms-scraper-rvny-cf63mhc3f-harisnasir1s-projects.vercel.app"
+               "https://cms-scraper.vercel.app/"
            )
            .AllowAnyHeader()
            .AllowAnyMethod()
