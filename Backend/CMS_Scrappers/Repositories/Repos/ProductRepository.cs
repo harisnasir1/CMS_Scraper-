45451558742c5bd7f81f5df77daec2279a9159f1
harisnasir1 <haris.nasir0101@gmail.com>
2025-08-12 17:26:51 +0100

    Review product enhanced fillter

--- Backend/CMS_Scrappers/Repositories/Repos/ProductRepository.cs
+++ Backend/CMS_Scrappers/Repositories/Repos/ProductRepository.cs
@@ -24,10 +24,11 @@ namespace CMS_Scrappers.Repositories.Repos
         }
         public async Task<List<Sdata>> GetPendingReviewproducts(int PageNumber, int PageSize)
         {
-            return await _context.Sdata
+           return await _context.Sdata
                   .Where(s =>  s.Status == "Categorized" && s.Condition == "New" && (s.Brand == "Chrome Hearts" || s.Brand == "Louis Vuitton")&& (s.ProductType != "" || s.Category!= ""))
                   .Include(s => s.Image)
                   .Include(s => s.Variants)
+                  .Where(s => s.Variants.Any(v => v.InStock))
                   .OrderByDescending(s => s.CreatedAt)
                   .Skip((PageNumber - 1) * PageSize)
                    .Take(PageSize)
