1292c762634d0f057e46c444469fc79bbbe882ac
Haris <haris.nasir0101@gmail.com>
2026-03-05 16:12:29 +0000

    disable scrapping api

--- Backend/CMS_Scrappers/Controller/ScraperController.cs
+++ Backend/CMS_Scrappers/Controller/ScraperController.cs
@@ -27,25 +27,25 @@ public class ScraperController : ControllerBase
         return Ok("Scraper is ready");
     }
 
-    [HttpGet("test-scrape")]
-    public async Task<IActionResult> TestScrape()
-    {
-        try
-        {
-             _taskQueue.QueueBackgroundWorkItem(async (serviceProvider, token) =>{
-                using var scope = serviceProvider.CreateScope();
-                var Shopifyscrfactory=scope.ServiceProvider.GetRequiredService<IShopifyScrapperFact>();
-                var scraper=Shopifyscrfactory.CreateScraper("savonches");
-                await scraper.ScrapeAsync();
-             });
-
-            return Ok("Scraping started - check console for results");
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, new { error = "Scraping failed", message = ex.Message });
-        }
-    }
+    //[HttpGet("test-scrape")]
+    //public async Task<IActionResult> TestScrape()
+    //{
+    //    try
+    //    {
+    //         _taskQueue.QueueBackgroundWorkItem(async (serviceProvider, token) =>{
+    //            using var scope = serviceProvider.CreateScope();
+    //            var Shopifyscrfactory=scope.ServiceProvider.GetRequiredService<IShopifyScrapperFact>();
+    //            var scraper=Shopifyscrfactory.CreateScraper("savonches");
+    //            await scraper.ScrapeAsync();
+    //         });
+//
+    //        return Ok("Scraping started - check console for results");
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        return StatusCode(500, new { error = "Scraping failed", message = ex.Message });
+    //    }
+    //}
 
     [HttpGet("Getallscrapers")]
 
